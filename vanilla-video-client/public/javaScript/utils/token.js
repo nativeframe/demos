a9b663b50a75b74629dc8a2ff538653c6934e9ad
jansedav <davidedjansen@gmail.com>
2023-12-14 17:24:22 -0600

    Functioning Vanilla Encoder

---
+++ vanilla-video-client/public/javaScript/utils/token.js
@@ -0,0 +1,54 @@
+async function fetchToken (authUrl, reqBody){
+    const response = await window.fetch(authUrl, {
+        method: "post",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqBody),
+    });
+    if (response.status !== 200) {
+        throw new Error("Unable to get token");
+    }
+
+    const body = await response.json();
+    return body.token;
+};
+
+async function tokenRefresher(options){
+    const mirrors =
+        options.scope === "conference-owner"
+        ? [
+            {
+                id: options.streamKey,
+                streamName: "demo",
+                kind: "rtmp",
+                rtmpPath: `/origin_proxy/${options.streamKey}`,
+                clientEncoder: "demo",
+                streamKey: options.streamKey,
+                clientReferrer: options.clientReferrer !== undefined ? options.clientReferrer : null,
+            },
+            ]
+        : undefined;
+
+    const url = `${options.authUrl}`;
+    let token;
+    try {
+        const fetchOptions = {
+            scopes: [options.scope],
+            userId: options.userId ?? options.streamKey,
+            data: {
+                displayName: options.displayName ?? options.streamKey,
+                mirrors,
+            },
+        };
+        token = await fetchToken(url, fetchOptions);
+    } catch (error) {
+        console.error("unable to get access token", {
+        error,
+        url,
+        });
+        throw error;
+    }
+    return token;
+};
+
