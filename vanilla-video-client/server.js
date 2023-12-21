e527166fce9de255b5c47209a846bbbe87e5f6f6
jansedav <davidedjansen@gmail.com>
2023-12-21 15:31:50 -0600

    Adding in LSI instead of token, partially broken

--- vanilla-video-client/server.js
+++ vanilla-video-client/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const browserSync = require('browser-sync');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
