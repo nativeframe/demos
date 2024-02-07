25498a20e86df3e4018a239ba804c932521d8b34
jansedav <davidedjansen@gmail.com>
2024-02-07 14:03:22 -0600

    Removing exports because vanilla doesn't like it

--- vanilla-video-client/public/javaScript/globalConfigs.js
+++ vanilla-video-client/public/javaScript/globalConfigs.js
@@ -1,2 +1,2 @@
-export const backendEndpoint = "Your backend endpoint"; // The URL for the environment you plan to stream to.
-export const serviceEndpoint = "Your service endpoint"; // The URL for the service you run in the concept-api folder.
+const backendEndpoint = "Your backend endpoint"; // The URL for the environment you plan to stream to.
+const serviceEndpoint = "Your service endpoint"; // The URL for the service you run in the concept-api folder.
