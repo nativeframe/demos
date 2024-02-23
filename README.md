a04d49aaefc64243b191ac45607dd449fcae135b
jasonamark <jason.a.mark@gmail.com>
2024-02-22 17:53:47 -0800

    Add logic to demonstrate webhooks.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# demos
-Demo Applications
+# Demos
+
+`-` Video Client used within a React framework  
+`-` Video Client implemented using vanilla JavaScript
+
+## Disclaimer
+
+These projects are for demonstration and educational purposes only. It is not intended for use in a production environment. While every effort has been made to ensure the quality and accuracy of the information provided, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the project or the information, products, services, or related graphics contained in these projects for any purpose. Any reliance you place on such information is therefore strictly at your own risk.
--- vanilla-video-client/public/javaScript/manifestPlayer.js
+++ vanilla-video-client/public/javaScript/manifestPlayer.js
@@ -5,17 +5,17 @@ let vc = null;
 // Click handler for playing/pausing the video.
 function handlePlayerPlay() {
   // Ensure the player exists.
-    if (playerVideo) {
+  if (playerVideo) {
     // Changing the text of the playbutton to play/pause dependent of state.
     const video = document.getElementById("playerPlayButton");
     let text = video.textContent;
-    if(text === 'Pause'){
-      video.textContent = 'Play'
+    if (text === "Pause") {
+      video.textContent = "Play";
     } else {
-      video.textContent = 'Pause'
+      video.textContent = "Pause";
     }
     // Styling.
-    video.classList.toggle('highlight');
+    video.classList.toggle("highlight");
     // This is where the player is actually set to the play/paused state.
     playerVideo.localVideoPaused = !playerVideo.localVideoPaused;
   }
@@ -28,13 +28,13 @@ function handlePlayerMute() {
     // Changing the text of the mutebutton mute/unmute dependent of state.
     const mute = document.getElementById("playerMuteButton");
     let text = mute.textContent;
-    if(text === 'Mute'){
-      mute.textContent = 'Unmute'
+    if (text === "Mute") {
+      mute.textContent = "Unmute";
     } else {
-      mute.textContent = 'Mute'
+      mute.textContent = "Mute";
     }
     // Styling.
-    mute.classList.toggle('highlight');
+    mute.classList.toggle("highlight");
     // This is where the player is actually setting the muted/unmuted state.
     playerVideo.localAudioMuted = !playerVideo.localAudioMuted;
   }
@@ -49,45 +49,51 @@ function handlePlayerFullScreen() {
 }
 
 function addPlayer(player) {
-  //Here we are ensuring the Create Video Element feature exists on the current VideoClient.
+  // Here we are ensuring the Create Video Element feature exists on the current VideoClient.
   if (
     VideoClient.adapter.device.isImplements(
       VideoClient.adapter.Feature.CREATE_VIDEO_ELEMENT
     )
   ) {
     // Check to see if you already have a wrapper containing a player and remove it if so.
-    const wrapper = document.getElementById("playerWrapper")
+    const wrapper = document.getElementById("playerWrapper");
     if (wrapper) {
-        wrapper.remove();
+      wrapper.remove();
     }
     const container = document.getElementById("container");
-    
-    // If we don't have a wrapper the buttons havent been created yet, so lets create them.
+
+    // If we don't have a wrapper the buttons haven't been created yet, so lets create them.
     if (!wrapper) {
-        // Create and append the play/pause button.
-        const playerPlayButton = document.createElement("button");
-        playerPlayButton.id = "playerPlayButton";
-        playerPlayButton.textContent = "Pause";
-        container.appendChild(playerPlayButton);
+      // Create and append the play/pause button.
+      const playerPlayButton = document.createElement("button");
+      playerPlayButton.id = "playerPlayButton";
+      playerPlayButton.textContent = "Pause";
+      container.appendChild(playerPlayButton);
 
-        // Create and append the mute/unmute button.
-        const playerMuteButton = document.createElement("button");
-        playerMuteButton.id = "playerMuteButton";
-        playerMuteButton.textContent = "Mute";
-        container.appendChild(playerMuteButton);
+      // Create and append the mute/unmute button.
+      const playerMuteButton = document.createElement("button");
+      playerMuteButton.id = "playerMuteButton";
+      playerMuteButton.textContent = "Mute";
+      container.appendChild(playerMuteButton);
 
-        // Create and append the fullscreen button.
-        const playerFullScreenButton = document.createElement("button");
-        playerFullScreenButton.id = "playerFullScreenButton";
-        playerFullScreenButton.textContent = "FullScreen";
-        container.appendChild(playerFullScreenButton);
-      
-        //Event listeners to trigger our click handler functions when button elements are clicked.
-        document.getElementById("playerPlayButton").addEventListener("click", handlePlayerPlay);
-        document.getElementById("playerMuteButton").addEventListener("click", handlePlayerMute);
-        document.getElementById("playerFullScreenButton").addEventListener("click", handlePlayerFullScreen);
+      // Create and append the fullscreen button.
+      const playerFullScreenButton = document.createElement("button");
+      playerFullScreenButton.id = "playerFullScreenButton";
+      playerFullScreenButton.textContent = "FullScreen";
+      container.appendChild(playerFullScreenButton);
+
+      // Event listeners to trigger our click handler functions when button elements are clicked.
+      document
+        .getElementById("playerPlayButton")
+        .addEventListener("click", handlePlayerPlay);
+      document
+        .getElementById("playerMuteButton")
+        .addEventListener("click", handlePlayerMute);
+      document
+        .getElementById("playerFullScreenButton")
+        .addEventListener("click", handlePlayerFullScreen);
     }
-    
+
     // Create the Player Wrapper.
     const playerWrapper = document.createElement("div");
     playerWrapper.id = "playerWrapper";
@@ -101,22 +107,21 @@ function addPlayer(player) {
     // Finally we append our player to our playerWrapper.
     document.getElementById("playerWrapper").appendChild(videoEl);
   }
-
 }
 
 // Function to create our VideoClient instance and call to create our player.
 function createVideoClient(manifestUrl, VideoClient, endpoint) {
   // If we don't have a player yet we don't have a VideoClient instance and need to create one.
-  if(playerVideo === null){
+  if (playerVideo === null) {
     vc = new VideoClient.VideoClient({
-    backendEndpoints: [endpoint],
-  });
+      backendEndpoints: [endpoint],
+    });
 
-  // If we already have a player we need to dispose of the old one so we can replace it with a new one.
-  if(playerVideo != null){
-    playerVideo.dispose();
-    playerVideo = null;
-  }
+    // If we already have a player we need to dispose of the old one so we can replace it with a new one.
+    if (playerVideo != null) {
+      playerVideo.dispose();
+      playerVideo = null;
+    }
   }
   try {
     // Here is our actual request to create our player with our manifestUrl provided.
